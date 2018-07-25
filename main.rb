7eadfb7b72bc15ed91c51948186c9399de35ff99
Renae <renaejuvan@gmail.com>
2018-07-25 10:25:48 +1000

    second commit

--- main.rb
+++ main.rb
@@ -15,6 +15,24 @@ require_relative 'db_config'
 require_relative 'models/weekday'
 require_relative 'models/special'
 
+enable :sessions
+
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+  # if theres a current user, then return true, otherwise if not, return false
+  def logged_in? 
+    # !!current_user either returning true or false without if statement
+    if current_user #forcing current_user user object: true, or nil: false
+      true
+    else
+      false
+    end
+  end
+
+end
+
 get '/' do
   
     redirect '/happyhour'
@@ -51,19 +69,75 @@ post '/happyhour/special' do
   redirect '/happyhour'
 end
 
+
+
+
 # route to get to day where user has specified
 get '/day/:day' do
   user_specified_day = params[:day]
   @specials = Special.where(user_specified_day => true)
-  
   erb :day
 end
 
-# route to list particular venues and thier specials
-get '/special/:id' do
+# route to edit a special
+get '/special/:id/edit' do
   @specials = Special.find(params[:id])
+  erb :edit
+end
+
+put '/special/:id' do
+
+  special = Special.find(params[:id])
+  special.monday = params[:monday]
+  special.tuesday = params[:tuesday]
+  special.wednesday = params[:wednesday]
+  special.thursday = params[:thursday]
+  special.friday = params[:friday]
+  special.saturday = params[:saturday]
+  special.sunday = params[:sunday]
+  special.venue_name = params[:venue_name]
+  special.address = params[:address]
+  special.suburb = params[:suburb]
+  special.happy_hour_time = params[:happy_hour_time]
+  special.description = params[:description]
+  special.url = params[:url]
+  special.save
+
+  redirect "/special/#{ params[:id] }"
+end
+
+# route to list particular venues and their specials
+get '/special/:id' do
+  @special = Special.find(params[:id])
   erb :venue_details
 end
+
+get '/login' do
+  erb :login
+end
+
+post '/session' do
+  # grab email and password
+  # find the user by email
+  user = User.find_by(email: params[:email])
+  # authenticate user with password
+  if user && user.authenticate(params[:password])
+    #showing who is logged in
+    session[:user_id] = user.id
+    redirect '/'
+    # create new session
+    # redirect to secret page
+  else
+    erb :login
+  end
+end
+
+delete '/session' do
+  # end the session
+  session[:user_id] = nil    # redirect to login becuase we are doing a destructive operation. want to direct to a safe page, a get page
+  redirect '/login'
+end
+
 
 
 
